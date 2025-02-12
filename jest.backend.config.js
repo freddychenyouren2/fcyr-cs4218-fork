953c81cc886f57dce1c981ab3397bfe759d435cd
Freddy CYR <freddychenyouren2@gmail.com>
2025-02-12 15:20:25 +0800

    Set up files up to Lab 2

---
+++ jest.backend.config.js
@@ -0,0 +1,25 @@
+export default {  
+  // display name
+  displayName: "backend",
+
+  // when testing backend
+  testEnvironment: "node",
+
+  // which test to run
+  testMatch: ["<rootDir>/controllers/*.test.js"],
+
+  // jest does not recognise jsx files by default, so we use babel to transform any jsx files
+  transform: {
+    "^.+\\.jsx?$": "babel-jest",
+  },
+
+  // jest code coverage
+  collectCoverage: true,
+  collectCoverageFrom: ["controllers/**"],
+  coverageThreshold: {
+    global: {
+      lines: 100,
+      functions: 100,
+    },
+  },
+};
